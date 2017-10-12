6afbed6726fc43f94b9724f049a9e91531131330
Chan Tiu <b.kristhian.tiu@gmail.com>
2017-10-12 09:48:50 +0800

    Users and Roles : Initial UI

--- src/app/main/admin/admin-routing.module.ts
+++ src/app/main/admin/admin-routing.module.ts
@@ -9,6 +9,10 @@ const routes: Routes = [
 		path: 'users',
 		loadChildren: './users/users.module#UsersModule'
 	    },
+	    {
+		path: 'roles',
+		loadChildren: './roles/roles.module#RolesModule'
+	    },
 	    { path: '', redirectTo: 'users' }
 	]
     }
