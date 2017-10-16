4db7e90a00c226e4b0cdfdf67823647d6b63cf26
Chan Tiu <b.kristhian.tiu@gmail.com>
2017-10-16 19:19:01 +0800

    Users: - CREATE - input validations - show error from server

--- src/app/shared/_components/user-form.component.ts
+++ src/app/shared/_components/user-form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { NotificationsService } from 'angular2-notifications';
+
 import { User }           from 'app/core/models';
 import { UserService }    from 'app/core/services';
 
@@ -12,8 +14,12 @@ export class UserFormComponent implements OnInit {
 
   private user: User;
   private confirmation_password : String;
-    
-  constructor(private userService: UserService) {
+  private errors: any = {};
+  private has_errors = false;
+  private is_processing = false;
+
+  constructor(private userService: UserService,
+	      private _notificationsService: NotificationsService) {		
       this.user = new User('',false, '', '','','','','');
       this.confirmation_password = '';
   }
@@ -22,13 +28,29 @@ export class UserFormComponent implements OnInit {
   }
 
   onSaveClick(input_user: User){
-      console.log('HELOOOOOOOOOOOOOOOOOOOOOO');
+      this.errors = {};
+      this.has_errors = false;
+      this.is_processing = true;
       this.userService.create(input_user).subscribe(
           created_user => {
-              console.log(created_user, 'ROLE CREATED : user-form.component');
+	      this.is_processing = false;
+              console.log(created_user, 'USER CREATED : user-from.component');
+	      this._notificationsService.success(
+		  'New User : ' + input_user.username,
+		  'Successfully Created.',
+		  {
+		      timeOut: 10000,
+		      showProgressBar: true,
+		      pauseOnHover: false,
+		      clickToClose: false,
+		  }
+	      )
           },
           errors  => {
               console.log(errors, 'ERROR : user-form.component');
+	      this.errors = errors;
+	      this.has_errors = true;
+	      this.is_processing = false;
           });
   }//--onSaveClick
 }
