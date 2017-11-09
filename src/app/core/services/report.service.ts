1167b9a38fe8138620ffd74c56f4c51b075bfe28
psales <philip@alliedworld.healthcare>
2017-11-09 22:02:49 +0800

    Reports: PDF and CSV client side

--- src/app/core/services/report.service.ts
+++ src/app/core/services/report.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
-import { Http, Headers, RequestOptions, Response } from '@angular/http';
+import { Http, Headers, RequestOptions, Response, ResponseContentType } from '@angular/http';
 
+import { Observable } from 'rxjs';
 import 'rxjs/add/operator/toPromise';
+import 'rxjs/add/operator/map'
 
 import { Helper }         from '../helper';
 import { Report }       from '../models';
@@ -10,16 +12,63 @@ import { environment }    from 'environments/environment';
 @Injectable()
 export class ReportService {
 
+  private reportUrlVersion = 'v1';
+  private reportUrl = environment.API_ENDPOINT + '/reports'; 
+  
   constructor(private http: Http) {
   }//--constructor
 
-  getAll(): Promise<Report[]> {
-    const url = environment.API_ENDPOINT + '/reports/';
+  getReports(): Promise<Report[]> {
+    const url = `${this.reportUrl}`;
     return this.http.get(url)
                     .toPromise()
-                    .then(response => response.json().data as Report[])
+                    .then((response:Response) => 
+                    {
+                      console.info("getReports: ", response.json());
+                      return response.json().data as Report[];
+                    })
                     .catch(this.handleError);
-  }//--getAll
+  }//--getReports
+
+  getReport(id: number): Promise<Report> {
+    const url = `${this.reportUrl}/${id}`;
+    return this.http.get(url)
+                    .toPromise()
+                    .then((response:Response) => 
+                    {
+                      console.log(url);
+                      console.info("getReports: ", response.json());
+                      return response.json().data as Report;
+                    })
+                    .catch(this.handleError);
+  }//--getReport
+
+  downloadFileJSON() {
+    const url = `${this.reportUrl}`;
+
+    return this.http
+               .get(url)
+               .map((res) => {
+                 console.info("BODY: ", res['_body']);
+                 console.info("BODY: ", JSON.stringify(res['_body'].data));
+                 return new Blob([JSON.stringify(res['_body'].data)], {type: 'application/json'});
+               });
+  }
+
+  downloadFilePDF() {
+    const url = `${this.reportUrl}`;
+
+    return this.http
+               .get(url)
+               .map((res) => {
+                 console.info("BODY: ", res['_body']);
+                 console.info("BODY: ", JSON.stringify(res['_body'].data));
+                 return new Blob([JSON.stringify(res['_body'].data)], {type: 'application/pdf;charset=utf-8'});
+               });
+  }
+
+
+
 
   private handleError(error: any): Promise<any> {
     console.log('An error occured');
--- src/app/main/medical/reports/reports-list.component.ts
+++ src/app/main/medical/reports/reports-list.component.ts
@@ -3,6 +3,16 @@ import { Component, OnInit } from '@angular/core';
 import { ReportService } from 'app/core/services';
 import { Report } from 'app/core/models';
 
+import * as FileSaver from 'file-saver';
+
+import { Observable } from 'rxjs';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/toPromise';
+import 'rxjs/add/operator/map'
+
+//import * as jsPDF from 'jspdf';
+declare var jsPDF: any;
+
 @Component({
   selector: 'app-reports-list',
   templateUrl: './reports-list.component.html',
@@ -11,15 +21,66 @@ import { Report } from 'app/core/models';
 export class ReportListComponent implements OnInit {
 
   private reports : Report[];
-  constructor(private reportService: ReportService) { }
+  private selectedReport : Report;
+
+  constructor(
+    private reportService: ReportService
+  ) { }
 
   ngOnInit() {
     this.reportService
-        .getAll()
+        .getReports()
         .then(reports => {
           console.log(reports);
-          this.reports = reports;
+          return this.reports = reports;
         });
   }
+
+  view(id: number): void {
+    console.info("---id--", id);
+
+    this.reportService
+        .getReport(id)
+        .then(selectedReport => {
+          console.info("selectedReport: ", selectedReport);
+          this.selectedReport = selectedReport;
+        });
+  }
+
+
+  downloadJSON(): any {
+    this.reportService
+      .downloadFileJSON()
+      .subscribe((response) => {
+        FileSaver.saveAs(response, "test.json");
+      });
+  }
+
+  downloadPDF(): any {
+    this.reportService
+      .downloadFilePDF()
+      .subscribe((response) => {
+        FileSaver.saveAs(response, "test.pdf");
+      });
+  }
+
+  downloadJSPDF(): any {
+    const columns = [
+      { title: "ID", datakey: "id" },
+      { title: "Name", datakey: "name" }
+    ];
+    const data = [
+      { "id": 1, "name": "Shaw"},
+      { "id": 2, "name": "Shaws"}
+    ]
+
+    var doc = new jsPDF('p', 'pt');
+    doc.autoTable(columns,data);
+    doc.save("table.pdf");
+  }
+
+
+
+
 
 }
