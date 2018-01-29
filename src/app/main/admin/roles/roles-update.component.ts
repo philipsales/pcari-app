2a8ab16168452a1f8c1e3aaab22216db3ea5bcaa
Kristhian Tiu <b.kristhian.tiu@gmail.com>
2018-01-29 16:57:33 +0800

    Add Update Role - but can only view role

--- src/app/main/admin/roles/roles-update.component.ts
+++ src/app/main/admin/roles/roles-update.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import 'rxjs/add/operator/switchMap';
+
+import { RoleService } from 'app/core/services';
+
+import { Role } from 'app/core/models';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { NoJWTError } from 'app/core/errors';
+import { RoleJSON } from 'app/core/interfaces';
 
 @Component({
   selector: 'app-roles-update',
@@ -6,10 +14,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./roles-update.component.css']
 })
 export class RolesUpdateComponent implements OnInit {
+  private role: Role;
+  private role_id: string;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private roleService: RoleService
+  ) {
+    this.role = new Role('', '', true);
+  }
 
   ngOnInit() {
+    this.role_id = this.route.snapshot.paramMap.get('id');
+    this.roleService.get(this.role_id).subscribe((response: Role) => {
+      delete this.role;
+      this.role = response;
+      console.log(response);
+    }, error => {
+      console.log(error); // get the error in error handler
+      if (error instanceof NoJWTError) {
+        console.warn('TO DO : handle JWT Expired');
+      }
+    });
   }
 
 }
