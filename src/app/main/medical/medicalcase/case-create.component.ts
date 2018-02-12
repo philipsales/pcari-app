d0ca080b29a19281676c171eda01a810f251cc0c
Kristhian Tiu <b.kristhian.tiu@gmail.com>
2018-02-12 09:27:30 +0800

    apply shared picaricase-manage to Medical case create

--- src/app/main/medical/medicalcase/case-create.component.ts
+++ src/app/main/medical/medicalcase/case-create.component.ts
@@ -2,10 +2,13 @@ import { Component, OnInit } from '@angular/core';
 
 import {
   Case,
-  FormAnswer
+  FormAnswer,
+  Form
 } from 'app/core/models';
 
 import { environment } from 'environments/environment';
+import { FormService, CaseService } from 'app/core/services';
+import { NotificationsService } from 'angular2-notifications';
 
 @Component({
   selector: 'app-case-create',
@@ -15,14 +18,45 @@ import { environment } from 'environments/environment';
 export class CaseCreateComponent implements OnInit {
 
   private case: Case;
+  private forms: Form[];
   private answers: FormAnswer[];
+  private is_processing = false;
 
-  constructor() {
+
+  constructor(
+    private formService: FormService,
+    private caseService: CaseService,
+    private _notificationsService: NotificationsService
+  ) {
     this.answers = [];
     this.case = new Case('', environment.ORG_MEDICAL, '', this.answers);
   }
 
   ngOnInit() {
+    this.formService.getMedicalForms().subscribe(
+      forms => {
+        this.forms = forms;
+      }
+    );
   }
 
+  onSubmitCase(case_for_create: Case) {
+    this.caseService.create(case_for_create).subscribe((created_case: Case) => {
+      this.is_processing = false;
+      console.log(created_case, 'CASE CREATED : case-manage.component');
+      this._notificationsService.success(
+        'New Case : ' + created_case.case_nbr,
+        'Successfully Created.',
+        {
+          timeOut: 10000,
+          showProgressBar: true,
+          pauseOnHover: false,
+          clickToClose: false,
+        }
+      );
+    }, errors => {
+      console.log(errors, 'ERROR : case-manage.component');
+      this.is_processing = false;
+    });
+  }
 }
