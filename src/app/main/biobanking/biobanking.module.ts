cd41a346263726aceecc1c7100151342aea48597
Kristhian Tiu <b.kristhian.tiu@gmail.com>
2018-03-21 07:12:27 +0800

    permissions for biobanking and medical plus admin missed

--- src/app/main/biobanking/biobanking.module.ts
+++ src/app/main/biobanking/biobanking.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { SharedModule } from 'app/shared/shared.module';
+
 import { BiobankingRoutingModule } from './biobanking-routing.module';
 import { BiobankingComponent } from './biobanking.component';
 
@@ -8,6 +10,7 @@ import { BiobankingComponent } from './biobanking.component';
 @NgModule({
   imports: [
     CommonModule,
+    SharedModule,
     BiobankingRoutingModule
   ],
   declarations: [BiobankingComponent]
--- src/app/main/medical/medical.module.ts
+++ src/app/main/medical/medical.module.ts
@@ -4,12 +4,15 @@ import { CommonModule } from '@angular/common';
 import { MedicalRoutingModule } from './medical-routing.module';
 import { MedicalComponent } from './medical.component';
 
+import { SharedModule } from 'app/shared/shared.module';
+
 import { MedicalMenuComponent } from './medical-menu.component';
 import { MedicalMenuTitleComponent } from './medical-menu-title.component';
 
 @NgModule({
   imports: [
     CommonModule,
+    SharedModule,
     MedicalRoutingModule
   ],
   declarations: [
