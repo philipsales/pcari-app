a8aaf05e988b33aa2c6afa4d69ca8e62f4c08b33
Kristhian Tiu <b.kristhian.tiu@gmail.com>
2018-03-22 12:30:20 +0800

    settings for prod included

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,17 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
 export const environment = {
-  production: true,
+  production: false,
   API_QUESTION_HOST: 'http://localhost:8888',
   API_QUESTION_ENDPOINT: 'http://localhost:8888/default',
-  API_HOST: 'http://localhost:3001',
-  // API_HOST: 'http://kristhiantiu.com:3000',
-  API_ENDPOINT: 'http://localhost:3001/',
-  // API_ENDPOINT: 'http://kristhiantiu.com:3000/',
-  API_ALLOWED_DOMAINS: ['localhost:3001', 'kristhiantiu.com:3000'],
+  // API_HOST: 'http://localhost:3001',
+  API_HOST: 'http://ewan.kristhiantiu.com',
+  // API_ENDPOINT: 'http://localhost:3001/',
+  API_ENDPOINT: 'http://ewan.kristhiantiu.com/',
+  API_ALLOWED_DOMAINS: ['localhost:3001', 'kristhiantiu.com:3000', 'ewan.kristhiantiu.com', 'test.kristhiantiu.com'],
   ORG_BIOBANK: 'University of the Philippines Diliman',
   ORG_MEDICAL: 'University of the Philippines - Philippine General Hospital',
   DEPT_MEDICAL: 'General Surgery Department',
